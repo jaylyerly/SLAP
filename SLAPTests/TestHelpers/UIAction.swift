7484ca26f8239a0d1d2131dd7ca63043c4c857c1
Jay Lyerly <1743037+jaylyerly@users.noreply.github.com>
2024-10-26 18:09:37 -0400

    Clean up

--- SLAPTests/TestHelpers/UIAction.swift
+++ SLAPTests/TestHelpers/UIAction.swift
@@ -11,15 +11,16 @@ extension UIAction {
 
     // Notes: Low level cheat for executing a UIAction, only
     // appropriate for testing
-    
+    private typealias ActionHandlerBlock = @convention(block) (UIAction) -> Void
+
     var handler: UIActionHandler {
-        typealias ActionHandlerBlock = @convention(block) (UIAction) -> Void
         let handler = value(forKey: "handler") as AnyObject
         return unsafeBitCast(handler, to: ActionHandlerBlock.self)
     }
-    
+
     /// Run the UIActionHandler
     func execute() {
         handler(self)
     }
+
 }
