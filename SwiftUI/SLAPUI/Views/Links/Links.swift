21243d97d43484fc76038c682092e38408b2fd00
Jay Lyerly <1743037+jaylyerly@users.noreply.github.com>
2025-04-10 17:07:58 -0400

    Flesh out the links screen

--- SwiftUI/SLAPUI/Views/Links/Links.swift
+++ SwiftUI/SLAPUI/Views/Links/Links.swift
@@ -8,7 +8,61 @@
 import SwiftUI
 
 struct Links: View {
+    
+    @Environment(\.config)
+    var config: Config
+    
     var body: some View {
-        Text("Links")
+        ScrollView {
+            VStack {
+                Text("Links")
+                    .font(config.largeTitleFont)
+                    .foregroundStyle(.white)
+                HStack {
+                    Image("Banner")
+                        .resizable()
+                        .scaledToFit()
+                        .accessibilityHidden(true)
+                }
+                .background(.white)
+                .cornerRadius(10)
+                .padding(10)
+                Spacer()
+                HStack {
+                    Link(destination: config.homeUrl) {
+                        VStack {
+                            Image(systemName: "house")
+                                .font(.system(size: 96))
+                                .accessibilityHidden(true)
+                            Spacer()
+                            Text("WebSite")
+                                .foregroundStyle(.white)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    
+                    Link(destination: config.storeUrl) {
+                        VStack {
+                            Image(systemName: "storefront")
+                                .font(.system(size: 96))
+                                .accessibilityHidden(true)
+                            Spacer()
+                            Text("Store")
+                                .foregroundStyle(.white)
+                            
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    
+                }
+                .padding(10)
+            }
+        }
+        .background(.slapBlue)
     }
+}
+
+#Preview {
+    Links()
+        .environment(\.config, Config())
 }
