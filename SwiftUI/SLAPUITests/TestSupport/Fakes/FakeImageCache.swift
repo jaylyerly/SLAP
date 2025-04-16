c623f0dd7ec60da40ee6e9a74ec2b1673d70e763
Jay Lyerly <1743037+jaylyerly@users.noreply.github.com>
2025-04-16 11:58:25 -0400

    Add a fake for imageCache

---
+++ SwiftUI/SLAPUITests/TestSupport/Fakes/FakeImageCache.swift
@@ -0,0 +1,56 @@
+//
+//  FakeImageCache.swift
+//  SLAPUI
+//
+//  Created by Jay Lyerly on 4/16/25.
+//
+
+import Foundation
+@testable import SLAPUI
+import Testing
+
+class FakeImageCache: ImageCache {
+    
+    enum Mode {
+        case cacheAlwaysWorks
+        case cacheNeverWorks
+    }
+    
+    private var mode: Mode
+    private let imageData: Data
+    
+    init(mode: Mode) throws {
+        self.mode = mode
+        
+        let bundle = Bundle.testBundle
+        let url = try #require(bundle.url(forResource: "kermit", withExtension: "png"))
+        imageData = try Data(contentsOf: url)
+        
+        super.init(inMemory: true)
+    }
+    
+    override func imageDataFromCache(for url: URL) -> Data? {
+        mode == .cacheAlwaysWorks ? imageData : nil
+    }
+    
+    override func imageDataFromCacheOrDownload(for url: URL) async throws -> Data? {
+        switch mode {
+            case .cacheAlwaysWorks:
+                // return data immediately
+                return imageData
+            case .cacheNeverWorks:
+                // suspend for a bit and then deliver the data
+                try await Task.sleep(for: .milliseconds(100))
+                return imageData
+        }
+    }
+    
+}
+
+extension ImageCache {
+    
+    static func fake(mode: FakeImageCache.Mode) throws -> ImageCache {
+        try FakeImageCache(mode: mode)
+    }
+    
+}
